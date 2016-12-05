c74177f30b57979c4f9be72b43d0e27069e225d4
dydimitrov <dydimitrov86@gmail.com>
2016-12-05 21:17:25 +0200

    new versions for conflict

--- sumOfDigits/sumOfDigits.cs
+++ sumOfDigits/sumOfDigits.cs
@@ -15,8 +15,7 @@ namespace sumOfDigits
 
             while (number>0)
             {
-                sumOfDigits = sumOfDigits + number % 10;
-                number = number / 10;
+                
             }
             
             Console.WriteLine(sumOfDigits);
